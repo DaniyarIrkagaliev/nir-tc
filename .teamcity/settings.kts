f3a269632e5406f691fb7db045e02484580d6f81
Данияр Иркагалиев <80203074+DaniyarIrkagaliev@users.noreply.github.com>
2025-05-25 19:58:36 +0300

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,2 +1,17 @@
-rootProjectId = "ProjectName"
 version = "2023.05"
+
+// 1. Сначала объявляем VCS root
+vcsRoot {
+    id("nir-tc-git") // Уникальный ID
+    name("NIR-TC Git") // Произвольное имя (не ссылка на файл)
+    url("https://github.com/DaniyarIrkagaliev/nir-tc.git")
+   
+    branchSpec = "+:refs/heads/master"
+    checkoutSubdir = ".teamcity" // Если конфиги в поддиректории
+}
+
+// 2. Затем объявляем проект
+project {
+    // Настройки проекта будут автоматически подхватывать .kts файлы
+    // из указанного VCS root
+}
