9f3eac0367526eb6196d12a007d0cf9ab95a5d21
Данияр Иркагалиев <80203074+DaniyarIrkagaliev@users.noreply.github.com>
2025-05-26 08:08:37 +0300

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,31 +1,15 @@
-version = "2024.12.2"
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.csharpScript
 
-// 1. Сначала объявляем VCS root
-vcsRoot {
-    id("nir-tc-git") // Уникальный ID
-    name("NIR-TC Git") // Произвольное имя (не ссылка на файл)
-    url("https://github.com/DaniyarIrkagaliev/nir-tc.git")
-   
-    branchSpec = "+:refs/heads/master"
-    checkoutSubdir = ".teamcity" // Если конфиги в поддиректории
-}
-
-// 2. Затем объявляем проект
-project {
-    versionedSettings {
-        vcsRootId = "NirTc_HttpsGithubComDaniyarIrkagalievNirTc"
-        settingsFormat = VersionedSettings.Format.KOTLIN
-        buildSettingsMode = VersionedSettings.BuildSettingsMode.PREFER_VCS
-    }
-    buildType {
-        id("ExampleBuild")
-        name("Example Build")
-        steps {
-            script {
-                name = "Test Step"
-                scriptContent = "echo 'Hello from TeamCity'"
-            }
+object Build : BuildType({
+    name = "Build"
+    // ...
+    steps {
+        csharpScript {
+            id = "csharpScript"
+            content = """Console.WriteLine("Running a CSharp script...");"""
+            tool = "%\teamcity.tool.TeamCity.csi.DEFAULT%"
         }
     }
-}
-
+    // ...
+})
